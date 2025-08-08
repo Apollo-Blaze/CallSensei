2632df7d931a42495f3e2731e27a70ab05b35542
AlnaElsa2004 <alnaelsat@gmail.com>
2025-08-08 10:29:31 +0530

    Added Navbar and AI component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Sidebar from "./components/sidebar/Sidebar";
 import MainWindow from "./components/MainWindow";
 
+
+
 const App: React.FC = () => {
   const [selectedId, setSelectedId] = useState<string | null>(null);
   const [aiExplanation, setAIExplanation] = useState<string>("");
